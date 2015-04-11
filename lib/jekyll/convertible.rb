jekyll/jekyll

commit 4b108df3ab99c9637d881a0b50027e5a4d5ad27f

convertible: abort with an error if Page/Post#data isn't a hash.
when the default_proc was being assigned, it failed if it wasn't a Hash. We
expect data to be a Hash everywhere, so let's freak out if it isn't after
reading and applying the fallback.

Fixes #3643.

lib/jekyll/convertible.rb
@@ -56,6 +56,10 @@ module Jekyll
       end
 
       self.data ||= {}
+
+      unless self.data.is_a?(Hash)
+        Jekyll.logger.abort_with "Fatal:", "Invalid YAML front matter in #{File.join(base, name)}"
+      end
     end
 
     # Transform the contents based on the content type.

patricker/jekyllrs

commit 52e9d569fb4ac8c57adf4db7372fa28d59821671

Convertible#read_yaml should return self.data.
Broken in 4b108df3ab99c9637d881a0b50027e5a4d5ad27f.

lib/jekyll/convertible.rb
@@ -60,6 +60,8 @@ module Jekyll
       unless self.data.is_a?(Hash)
         Jekyll.logger.abort_with "Fatal:", "Invalid YAML front matter in #{File.join(base, name)}"
       end
+
+      self.data
     end
 
     # Transform the contents based on the content type.